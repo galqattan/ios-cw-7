63fd011a319e7e031de589c11d0f414c9e5b34b9
galqattan <g_alqattan@icloud.com>
2020-07-05 01:35:03 +0300

    classwork 7 + bonus

---
+++ IOSClasswork7/MoviesMasterVC.swift
@@ -0,0 +1,30 @@
+//
+//  MoviesMasterVC.swift
+//  IOSClasswork7
+//
+//  Created by ghadeer alqattan on 7/4/20.
+//  Copyright © 2020 Safeyah Coding. All rights reserved.
+//
+
+import UIKit
+
+class MoviesMasterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
